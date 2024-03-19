4cc621f89794a814b7c62364fb9ceba5fc0564fc
MatheusKristman <kristman058@gmail.com>
2024-03-19 12:14:24 -0300

    fixed tablet version for services

--- app/(website)/_components/services.tsx
+++ app/(website)/_components/services.tsx
@@ -1,62 +1,34 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 import { ServicesCard } from "./services-card";
 import {
   ServicesTitleAnimation,
   ServicesContainerAnimation,
 } from "@/constants/framer/services-animations";
-
-const ServicesData = [
-  {
-    title: "Tv touch",
-    desc: "Com nossos projetos em TV Touch, você pode criar um ambiente de vendas envolvente e memorável para seus clientes.",
-    imageUrl: "/images/tv-touch.webp",
-    className: "sm:col-start-1 sm:col-end-3",
-    imageClassName: "!mb-4",
-    width: 512,
-    height: 288,
-  },
-  {
-    title: "Realidade Virtual",
-    desc: "Permita que seus clientes explorem e interajam com o empreendimento de forma imersiva e envolvente.",
-    imageUrl: "/images/vr.png",
-    className: "",
-    imageClassName: "!mb-4",
-    width: 512,
-    height: 288,
-  },
-  {
-    title: "Nuvem",
-    desc: "Nossos projetos na nuvem permitem que seus clientes acessem diretamente a partir de seus dispositivos.",
-    imageUrl: "/images/nuvem.png",
-    className: "",
-    imageClassName: "!mb-4",
-    width: 512,
-    height: 288,
-  },
-  {
-    title: "Projetos Externos",
-    desc: "Com os projetos externos, é possível visualizar o entorno e as comodidades do empreendimento, proporcionando uma visão completa do ambiente ao redor.",
-    imageUrl: "/images/projetos-externos.png",
-    className: "",
-    imageClassName: "!mb-4",
-    width: 512,
-    height: 288,
-  },
-  {
-    title: "Projetos Internos",
-    desc: "Os ambientes internos proporcionam aos clientes a chance de deslumbrar-se com o novo imóvel, juntamente com a opção de personalização.",
-    imageUrl: "/images/projetos-internos.png",
-    className: "",
-    imageClassName: "!mb-4",
-    width: 300,
-    height: 288,
-  },
-];
+import { ServicesData } from "@/constants/services-data";
 
 export function Services() {
+  const [services, setServices] = useState<typeof ServicesData>(ServicesData);
+
+  useEffect(() => {
+    if (window.innerWidth < 1024) {
+      const serv = [...services];
+
+      serv[0].imageUrl = "/images/tv-touch.png";
+
+      setServices(serv);
+    } else {
+      const serv = [...services];
+
+      serv[0].imageUrl = "/images/tv-touch.webp";
+
+      setServices(serv);
+    }
+  }, []);
+
   return (
     <motion.section
       initial="initial"
@@ -73,7 +45,7 @@ export function Services() {
       </motion.h2>
 
       <div className="w-full grid grid-cols-1 grid-rows-[repeat(5,auto)] gap-4 sm:grid-cols-2 sm:grid-rows-[repeat(3,auto)] lg:grid-rows-[repeat(2,auto)] lg:grid-cols-3">
-        {ServicesData.map((service, index) => (
+        {services.map((service, index) => (
           <ServicesCard
             key={index}
             title={service.title}
